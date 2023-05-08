3235d36baff15a7ef1456efd787262965e33963a
Speroce <speroce@yandex.ru>
2023-05-08 23:18:22 +0700

    mobx types

---
+++ src/lib/mobx.d.ts
@@ -0,0 +1,8 @@
+import * as _mobx from 'mobx';
+import * as _mobxreactlite from 'mobx';
+import * as _mobxreact from 'mobx';
+declare global {
+    const mobx: typeof _mobx;
+    const mobxReactLite: typeof _mobxreactlite;
+    const mobxReact: typeof _mobxreact;
+}
